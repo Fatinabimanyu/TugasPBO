ba2da0c681529ce09665af6e15bdef07e148b00e
Fatinabimanyu <79156679+Fatinabimanyu@users.noreply.github.com>
2021-04-29 00:26:34 +0700

    Add files via upload

---
+++ UTSPBOFatin/IMenu.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace PesenMinum
+{
+    public interface IMenu
+    {
+        string name {get; }
+        int price {get; }
+    }
+}
---
+++ UTSPBOFatin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PesenMinum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuScreen menu = new MenuScreen();
+
+            menu.Excecute2();
+
+        }
+    }
+}
